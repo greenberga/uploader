126af722d9255d2dbd412d7c94295d7682ea8d9e
Aaron Greenberg <aaron@greenberg.io>
2017-05-15 19:51:27 -0400

    Add tests for 'make_post()'

--- test_server.py
+++ test_server.py
@@ -1,4 +1,7 @@
+import datetime
 import os
+from unittest.mock import patch, mock_open
+
 from nose.tools import ok_
 
 def setup():
@@ -24,3 +27,71 @@ def test_format_summary():
 
     for summary, expected in specs.items():
         yield ok_, format_summary(summary), expected
+
+def test_make_post():
+
+    from server import make_post
+
+    today = datetime.date.today()
+    str_today = str(today)
+    nice_today = '{d:%B} {d.day}, {d:%Y}'.format(d = today)
+
+    specs = [
+
+        # WITH NO PASSED DATE OR SUMMARY
+        (
+            ('123', None, 'POSTCONTENT', None),
+            'blog/_posts/%s-123.md' % str_today,
+            r'''---
+layout: post
+---
+
+<p>
+  <time><a href="/123">%s</a></time>
+  <a href="/123">POSTCONTENT</a>
+</p>
+''' % nice_today,
+        ),
+
+        # WITH PASSED DATE
+        (
+            ('234', '2017-03-27', 'POSTCONTENT', None),
+            'blog/_posts/%s-234.md' % str_today,
+            r'''---
+layout: post
+---
+
+<p>
+  <time><a href="/234">March 27, 2017</a></time>
+  <a href="/234">POSTCONTENT</a>
+</p>
+''',
+        ),
+
+        # WITH PASSED SUMMARY
+        (
+            ('123', None, 'POSTCONTENT', 'A post about content'),
+            'blog/_posts/%s-123.md' % str_today,
+            r'''---
+layout: post
+---
+
+<p>
+  <time><a href="/123">%s</a></time>
+  <a href="/123">POSTCONTENT</a><span>A post about content</span>
+</p>
+''' % nice_today,
+        ),
+
+    ]
+
+    for args, xpected_fname, xpected_content in specs:
+        yield check_make_post, make_post, args, xpected_fname, xpected_content
+
+def check_make_post(make_post, args, xpected_fname, xpected_content):
+    m = mock_open()
+    with patch(make_post.__module__ + '.open', m, create = True):
+        make_post(*args)
+        m.assert_called_once_with(xpected_fname, 'w')
+        handle = m()
+        handle.write.assert_called_once_with(xpected_content)
